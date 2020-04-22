814a81deb9db79bb8acd112a21de751bd32cef75
Johnathan Dale <jdale@dev5.turtle.oi.io>
2020-04-21 23:52:35 -0400

    mod to insideout.bash

--- insideOutside.bash
+++ insideOutside.bash
@@ -3,13 +3,17 @@
 
 var1='A'
 var2='B'
+
 echo
 echo "------> Before the function call var1: $var1, var2: $var2"
+my_func
+echo "------> After the function call var1: $var1, var2: $var2"
+echo
+
+
 function my_func {
 local var1='C'
 var2='D'
 echo "------> Inside the function var1: $var1, var2: $var2"
 }
-my_func
-echo "------> After the function call var1: $var1, var2: $var2"
-echo
+
