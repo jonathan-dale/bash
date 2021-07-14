1817352f553c5665ca67d20e6e3e39e315134ad8
jonathan-dale <dalej1846@gmail.com>
2021-07-14 12:52:55 -0400

    source standard file for error handeling

---
+++ abort_die_info.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+abort() {
+	EXIT_VAL="$?"
+	echo "ABORT ERROR: exit code $EXIT_VAL occured, failed to execute '$BASH_COMMAND' on line '${BASH_LINENO[0]}' "
+	exit "$EXIT_VAL"
+}
+
+
+die() {
+	MESS="$1"
+	EXIT_VAL="${2:-1}"
+	echo '[DIE] ' "$MESS" 1>&2
+	exit "$EXIT_VAL"
+}
+
+
+info() {
+        CALL_TIME=`date +%M%S`
+        DIFF=`expr $CALL_TIME - $START_TIME`
+	echo '[INFO]['$DIFF'] ' "$@"
+}
+
+
+# Stop execution on any error, call abort, include message
+trap abort ERR
+
+START_TIME=`date +%M%S`
+
+AM_I_ROOT=`id -u`
+[[ "$AM_I_ROOT" == 0 ]] || die "must be root to run this script..."
